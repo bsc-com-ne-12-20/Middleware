b00804d1049eced8b325bdd841fd52b0a369441b
bsc-com-ne-01-20 <bsc-com-ne-01-20@unima.ac.mw>
2025-03-03 17:21:19 +0200

    url

--- middleware/settings.py
+++ middleware/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s8r#l8lfra=k!9e=++p5hhn5rblilcuc%fs*#k#=168tp9-m%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
