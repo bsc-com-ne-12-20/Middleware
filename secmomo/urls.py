5558d781500d1febed5b92feb1c36ee9641e333a
bsc-com-ne-11-20 <100414663+bsc-com-ne-11-20@users.noreply.github.com>
2025-05-01 00:50:17 +0200

    resoving coflicts (#23)
    * Deposit the money for client api
    
    * Remove external deposit api in deposit agent
    
    * modiefid
    
    * Modified the deposit api
    
    * added get agent username endpoint
    
    * Remove binary files and resolve merge conflict
    
    * aded get balance for an agent code
    
    * aded get balance for an agent code

--- secmomo/urls.py
+++ secmomo/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from .views import (
-<<<<<<< HEAD
-    auto_approve_agent, change_password, get_agent_username, get_balance,  register_agent, agent_login, 
-=======
-    auto_approve_agent, change_password, get_agent_username, register_agent, agent_login, 
->>>>>>> upstream/main
-    agent_logout, agent_profile
+    auto_approve_agent, change_password, get_agent_username, get_balance,  register_agent, agent_login, agent_logout, agent_profile
 )
 
 urlpatterns = [
@@ -14,12 +9,8 @@ urlpatterns = [
     path('logout/', agent_logout, name='agent-logout'),
     path('change-password/', change_password, name='change-password'),
     path('profile/', agent_profile, name='agent-profile'),
-<<<<<<< HEAD
     path('get-username/', get_agent_username, name='get_agent_username'),
     path('agent-balance/', get_balance, name='get-balance'),
-=======
-    path('agent/get-username/', get_agent_username, name='get_agent_username'),
->>>>>>> upstream/main
     path('applications/auto-approve/', auto_approve_agent, name='auto-approve-agent'),
 
 ]
