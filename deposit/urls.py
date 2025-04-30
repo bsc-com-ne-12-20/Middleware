870d3f61d9a8e762a93edbf717d6984cfa89fe72
bsc-com-ne-11-20 <bsc-com-ne-11-20@unima.ac.mw>
2025-04-30 19:21:50 +0200

    Modified the deposit api

--- deposit/urls.py
+++ deposit/urls.py
@@ -5,9 +5,6 @@ from .views import AgentDepositAPIView, AgentDepositHistoryAPIView
 
 urlpatterns = [
     path('dpst/', AgentDepositAPIView.as_view(), name='agent-deposit'),
-<<<<<<< HEAD
     path('dpst-hstr/', AgentDepositHistoryAPIView.as_view(), name='agent-deposit-history')
-=======
-    path('dpst-hstr/', AgentDepositHistoryAPIView.as_view(), name='agent-deposit-history'),
->>>>>>> 53f607d (Remove external deposit api in deposit agent)
+
 ]
