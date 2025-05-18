42548b6ae06991c3e9478f4d32b2b342a06e973b
Consider Lonjezo <127178172+bsc-com-ne-01-20@users.noreply.github.com>
2025-05-18 09:46:47 +0200

    add API endpoint for accepting money from paychangu gateway (#44)
    * Adding Middleware Agent project folder
    
    * Adding README file for admin user name and password
    
    * modified some parameters
    
    * modifie some parameters to align with frontend
    
    * adding commission and transaction percentage growth
    
    * delete mistaken created project within a project
    
    * removing undefined apps
    
    * modifies withdarw endpoint to work as needed
    
    * fixing some erros
    
    * modifying urls so they work inhand with frontend
    
    * add requests in requirements.txt
    
    * fixing withdraw for 500 error
    
    * fixing some limits of money
    
    * fixing some limits of money
    
    * adding api endpoint to allow deposit from paychangu gateway
    
    ---------
    
    Co-authored-by: Clement Masauko <bsc-com-ne-04-20@uniman.ac.mw>
    Co-authored-by: Clement Masauko <127254404+bsc-com-ne-04-20@users.noreply.github.com>

--- UserWithdrawsUsingAgent/urls.py
+++ UserWithdrawsUsingAgent/urls.py
@@ -1,7 +1,9 @@
+# UserWithdrawsUsingAgent/urls.py
 from django.urls import path
-from .views import UserWithdrawToAgentAPIView, AgentWithdrawalHistoryAPIView
+from .views import UserWithdrawToAgentAPIView, AgentWithdrawalHistoryAPIView, AgentWithdrawalDepositAPIView
 
 urlpatterns = [
     path('', UserWithdrawToAgentAPIView.as_view(), name='withdrawal'),
     path('wdr-to-agent-hstr/', AgentWithdrawalHistoryAPIView.as_view(), name='withdrawal-history'),
+    path('dpst/', AgentWithdrawalDepositAPIView.as_view(), name='withdraw-deposit'),
 ]
